0fae713aac82ea90b79f94d57cb8ff341dd56cf2
git <larker@foxmail>
2020-04-01 01:30:56 +0800

    20200401

---
+++ static/js/chunk-2d2371fe.4884d53e.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2371fe"],{fa7d:function(n,e,i){"use strict";i.r(e),i.d(e,"getApiUrl",(function(){return c}));var t="http://ximyan.free.idcfengye.com/dev-api";function c(){return t}}}]);
